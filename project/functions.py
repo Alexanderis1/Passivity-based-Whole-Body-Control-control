3bfe934ef7697a29d3712fb16bf81f5af9e64b0c
DanieleTeni <teni.2115180@studenti.uniroma1.it>
2025-03-17 17:53:49 +0100

    correcting error fk

--- project/functions.py
+++ project/functions.py
@@ -10,26 +10,31 @@
 
 # 3 Step
 
-
-def compute_desired_force(tasks,pos_error,vel_error,ff,gravity_torque,current) :
+import numpy as np
+def compute_desired_force(tasks,pos_error,vel_error,ff,gravity_torque,current,Jdot,M) :
 
     "compute the desirede force f_desired as the stack of the fk forces"
     # task = ['lfoot', 'rfoot', 'com', 'torso', 'base', 'joints']  as in the origina file 
     #same for pos_error, vel_error, ff  
 
+    f_des = np.array([]).reshape(0, 1)
 
-    #
 
-    num_task = len(tasks)
-    f_des=np.zeros(num_task)
-    for i,task in enumerate(tasks) :
-        Mk= compute_Mk(task)                                  #  I assume the other code have some function like that , when will merge code 
-        Tk= compute_Tk(task)                                  # we eventually change the input of that function according to yours 
+    
+    for task in tasks :
+        
+        # Mk= compute_Mk(task)                                  #  I assume the other code have some function like that , when will merge code 
+        # Tk= compute_Tk(task)                                # we eventually change the input of that function according to yours 
+        
+        
+        Mk,Tk=compute_task_space_matrices(Jdot[task], M)    #according to Alessandro function  
+
         Dk= compute_Dk(task)
         Ck= compute_Ck(task)
         Qk= compute_Qk(task)
         Kk= compute_Kk(task)
-        f_des[i] = Tk@gravity_torque + Mk@Qk@current['joint']['vel'] + Mk @ ff[task] + (Ck +Dk) @ vel_error[task] +Kk@ pos_error[task]
+        f=Tk@gravity_torque + Mk@Qk@current['joint']['vel'] + Mk @ ff[task] + (Ck +Dk) @ vel_error[task] +Kk@ pos_error[task]
+        f_des = np.vstack((f_des, f.reshape(-1, 1)))
     return f_des
 
 
